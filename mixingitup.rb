2b82c616c88e1b160d35a300e77121ba9fb80a1f
Maggie Ho <maggieho@gmail.com>
2014-02-21 02:41:29 -0800

    initial commit

---
+++ mixingitup.rb
@@ -0,0 +1,7 @@
+puts 'Bonjour! What\'s your first name?'
+first_name = gets.chomp
+puts 'May I also have your middle name?'
+middle_name = gets.chomp
+puts 'All right, one more thing.  Please tell me your last name.'
+last_name = gets.chomp
+puts 'Very nice to meet you, ' + first_name + ' ' + middle_name + ' ' + last_name + '! :)'
