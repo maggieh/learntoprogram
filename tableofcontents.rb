ea2b846d19140225aed3aa93e42a1f4c124522da
Maggie Ho <maggieho@gmail.com>
2014-02-22 01:52:02 -0800

    initial commit

---
+++ tableofcontents.rb
@@ -0,0 +1,7 @@
+line_width = 60
+half_width = line_width/2
+puts 'Table of Contents'.center line_width
+puts ''
+puts 'Champter 1:  Getting Started'.ljust(half_width) + 'page  1'.rjust(half_width)
+puts 'Champter 2:  Numbers'.ljust(half_width) + 'page  9'.rjust(half_width)
+puts 'Champter 3:  Letters'.ljust(half_width) + 'page  13'.rjust(half_width)
